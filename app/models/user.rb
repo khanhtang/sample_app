8fb9c59d31c9aca8d861963e9b5aff6d9b442dd7
Khanhtn <khanhtang1403@gmail.com>
2022-10-20 14:29:49 +0700

    Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.valid_email_regex
 
+  attr_accessor :remember_token
+
   before_save :downcase_email
   
   validates :name, presence: true, length: {maximum: Settings.maximum_name}
@@ -10,6 +12,36 @@ class User < ApplicationRecord
     length: {minimum: Settings.minimum_pass_length}
 
   has_secure_password
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+  
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false unless remember_token
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
 
   private
 
