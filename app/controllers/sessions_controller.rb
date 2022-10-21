889c4fec597f255185bce236667d4813c6018dcb
Khanhtn <khanhtang1403@gmail.com>
2022-10-21 09:05:40 +0700

    Chapter 11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase 
     if user &.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_to user
+      else
+        message = t "account_not_activated_check_your_email_for_the_activation_link"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t "invalid_email_password_combination"
       render :new
